e2433c6b15dff6f5a6dfe92d917b863d0a68e883
yusard <y.ardavardar@gmail.com>
2020-09-07 13:26:22 +0300

    H

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => (
     <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"></link>
     <div className="container">
-      <a className="navbar-brand" href="#">Prizebitz</a>
+      <a className="navbar-brand">Prizebitz</a>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
